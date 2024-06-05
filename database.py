23ffb21e52a09105ff70fdba358b9020b52c42b2
Abbas Mustafa <52071783+abbasmsh1@users.noreply.github.com>
2024-06-05 22:18:47 +0500

    updated schema

--- database.py
+++ database.py
@@ -10,15 +10,20 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Userengine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+class Master(Base):
+    __tablename__ = "master"
+    ITS = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    middle_name = Column(String, index=True)
+    last_name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     age = Column(Integer)
-
+    DOB = Column(Integer)
+    passport_No = Column(Integer, primary_key=True, index=True)
+    passport_Issue = Column(Integer, index=True)
+    passport_Expiry = Column(Integer, index=True)
 Base.metadata.create_all(bind=engine)
